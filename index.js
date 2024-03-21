20768a86bfb77dd1d47926464d32ae379fb11946
Githinji <ggathungu20905@gmail.com>
2024-03-21 17:00:13 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName='bob';
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase()
+}
+function setBestCustomer(){
+    bestCustomer='not bob';
+    
+}
+function overwriteBestCustomer(){
+     return bestCustomer='maybe bob'
+}
+
+     const leastFavouriteCustomer='Ann';
+     
+
+
+function changeLeastFavoriteCustomer(){
+    leastFavouriteCustomer='someone';
+}
